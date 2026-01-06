d1440bad80f5697f957a6fb570e9510702d3ecc6
Yako <111446097+YakoTheNeko@users.noreply.github.com>
2026-01-06 10:07:44 +0100

    Revert "le scriptable object"
    This reverts commit 1c889ce43a661958939757306bf4ed59dcd3286a.

---
+++ Assets/Scripts/BoatAutoPilot.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class BoatAutoPilot : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+    {
+        // On récupère notre vélocité initiale à partir de notre orientation dans le monde;
+        velocity = transform.forward;
+    }
+
+    private void Update()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, BoatManager.Singleton.neighborhoodRadius);
+        List<BoatAutoPilot> boats = colliders.Select(collider => collider.GetComponent<BoatAutoPilot>()).ToList();
+        boats.Remove(this);
+
+        Vector3 acceleration = ComputeAcceleration(boats);
+        UpdateVelocity(acceleration);
+        UpdatePosition(velocity);
+        UpdateRotation(velocity);
+    }
+
+    private Vector3 ComputeAcceleration(IEnumerable<BoatAutoPilot> boats)
+    {
+        Vector3 acceleration = Vector3.zero;
+
+        acceleration += ComputeAlignment(boats) * BoatManager.Singleton.alignmentAmount;
+        acceleration += ComputeSeparation(boats) * BoatManager.Singleton.separationAmount;
+        acceleration += ComputeCohesion(boats) * BoatManager.Singleton.cohesionAmount;
+
+        return acceleration;
+    }
+
+    private void UpdateVelocity(Vector3 acceleration)
+    {
+        velocity += acceleration;
+        velocity = LimitMagnitude(velocity, BoatManager.Singleton.maxSpeed);
+    }
+
+    private void UpdatePosition(Vector3 velocity)
+    {
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+    }
+
+    private void UpdateRotation(Vector3 velocity)
+    {
+        //transform.forward = velocity;
+        transform.forward = Vector3.RotateTowards(transform.forward, velocity, Time.deltaTime * BoatManager.Singleton.steeringSpeed, float.MaxValue);
+    }
+
+    private Vector3 ComputeAlignment(IEnumerable<BoatAutoPilot> boats)
+    {
+        var velocity = Vector3.zero;
+        if (!boats.Any()) return velocity;
+
+        foreach (var boat in boats)
+        {
+            velocity += boat.velocity;
+        }
+
+        velocity /= boats.Count();
+        var steer = Steer(velocity.normalized * BoatManager.Singleton.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 ComputeCohesion(IEnumerable<BoatAutoPilot> boats)
+    {
+        if (!boats.Any()) return Vector3.zero;
+
+        var sumPositions = Vector3.zero;
+        foreach (var boat in boats)
+        {
+            sumPositions += boat.transform.position;
+        }
+
+        var average = sumPositions / boats.Count();
+        var direction = average - transform.position;
+        var steer = Steer(direction.normalized * BoatManager.Singleton.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 ComputeSeparation(IEnumerable<BoatAutoPilot> boats)
+    {
+        var direction = Vector3.zero;
+        boats = boats.Where(boat => Vector3.Distance(transform.position, boat.transform.position) <= BoatManager.Singleton.separationRadius);
+        if (!boats.Any()) return direction;
+
+        foreach (var boat in boats)
+        {
+            Vector3 difference = transform.position - boat.transform.position;
+            direction += difference.normalized;
+        }
+
+        direction /= boats.Count();
+        var steer = Steer(direction.normalized * BoatManager.Singleton.maxSpeed);
+        return steer;
+    }
+
+    private Vector3 Steer(Vector3 desiredVelocity)
+    {
+        var steer = desiredVelocity - velocity;
+        steer = LimitMagnitude(steer, BoatManager.Singleton.maxForce);
+        return steer;
+    }
+
+    private Vector3 LimitMagnitude(Vector3 baseVector, float maxMagnitude)
+    {
+        if (baseVector.sqrMagnitude > maxMagnitude * maxMagnitude)
+        {
+            baseVector = baseVector.normalized * maxMagnitude;
+        }
+
+        return baseVector;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Skip if there's no BoatManager (e.g. in Prefab Edit mode)
+        if (BoatManager.Singleton == null)
+        {
+            return;
+        }
+
+        // Neighborhood radius.
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, BoatManager.Singleton.neighborhoodRadius);
+
+        // Separation radius.
+        Gizmos.color = Color.salmon;
+        Gizmos.DrawWireSphere(transform.position, BoatManager.Singleton.separationRadius);
+    }
+}
---
+++ Assets/Scripts/BoatManager.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatManager : MonoBehaviour
+{
+    private static BoatManager singleton = null;
+
+    public static BoatManager Singleton
+    {
+        get
+        {
+            return singleton;
+        }
+
+        private set
+        {
+            singleton = value;
+        }
+    }
+
+    [SerializeField]
+    private float width = 16f;
+
+    [SerializeField]
+    private float length = 9f;
+
+    [Range(0, 300)]
+    [SerializeField]
+    private int SpawningCount;
+
+    [SerializeField]
+    private GameObject boatHouseA = null;
+
+    [SerializeField]
+    private GameObject boatHouseB = null;
+
+    [SerializeField]
+    private GameObject boatHouseC = null;
+
+    [Range(0, 10)]
+    public float maxSpeed = 6f;
+
+    [Range(0.1f, 45f)]
+    public float steeringSpeed = 4.5f;
+
+    [Range(.01f, .5f)]
+    public float maxForce = .03f;
+
+    [Range(1, 10)]
+    public float neighborhoodRadius = 4f;
+
+    [Range(0.1f, 10f)]
+    public float separationRadius = 2.4f;
+
+    [Range(0, 3)]
+    public float separationAmount = 1.1f;
+
+    [Range(0, 3)]
+    public float cohesionAmount = 0.3f;
+
+    [Range(0, 3)]
+    public float alignmentAmount = 0.5f;
+
+    private List<GameObject> boatsInstances = new List<GameObject>();
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+
+    private void OnDestroy()
+    {
+        if (singleton == this)
+        {
+            singleton = null;
+        }
+    }
+
+    private void Start()
+    {
+        // On génère un nombre de bateau au départ.
+        for (int i = 0; i < SpawningCount; i++)
+        {
+            // On choisi une position et une orientation au hasard dans la zone de jeu.
+            Vector3 randomPosition = new Vector3((Random.value - 0.5f) * width, 0f, (Random.value - 0.5f) * length);
+            Quaternion randomRotation = Quaternion.Euler(0f, Random.value * 360f, 0f);
+            SpawnBoat(randomPosition, randomRotation);
+        }
+    }
+
+    private void SpawnBoat(Vector3 worldPosition, Quaternion worldRotation)
+    {
+        // Le bateau qu'on va instancier
+        GameObject boatToInstanciate = GetRandomBoat();
+
+        // Créer une instance (qu'on attache directement en enfant de notre transform)
+        GameObject boatInstance = Instantiate(boatToInstanciate, worldPosition, worldRotation, transform);
+
+        // Rajouter cette instance à notre liste d'instances
+        boatsInstances.Add(boatInstance);
+    }
+
+    private GameObject GetRandomBoat()
+    {
+        GameObject randomBoat = null;
+
+        // On prend une variable aléatoire entre 0.000 et 1.000
+        float randomValue = Random.value;
+        if (randomValue < 0.333f)
+        {
+            randomBoat = boatHouseA;
+        }
+        else if (randomValue < 0.666f)
+        {
+            randomBoat = boatHouseB;
+        }
+        else
+        {
+            randomBoat = boatHouseC;
+        }
+        // TODO Je ne vais pas rajouter un "if..else" pour chaque nouveau bateau ?!
+        // Il devrait y avoir un moyen de réunir mes Prefab dans une liste et
+        // d'en choisir un selon sa place dans la liste...
+
+        return randomBoat;
+    }
+
+    private void LateUpdate()
+    {
+        BorderPatrol();
+    }
+
+    private void BorderPatrol()
+    {
+        // On vérifie que nos bateaux sont dans la zone de jeu
+        // On les téléporte au côté opposé s'ils en sortent
+        int boatCount = boatsInstances.Count;
+        for (int i = 0; i < boatCount; i++)
+        {
+            GameObject boatInstance = boatsInstances[i];
+            Vector3 localPosition = boatInstance.transform.localPosition;
+            bool positionHasChanged = false;
+
+            // Left border?
+            if (localPosition.x < -width * 0.5f)
+            {
+                localPosition.x += width;
+                positionHasChanged = true;
+            }
+            // Right border?
+            else if (localPosition.x > width * 0.5f)
+            {
+                localPosition.x -= width;
+                positionHasChanged = true;
+            }
+
+            // Top border?
+            if (localPosition.z > length * 0.5f)
+            {
+                localPosition.z -= length;
+                positionHasChanged = true;
+            }
+            // Bottom border?
+            else if (localPosition.z < -length * 0.5f)
+            {
+                localPosition.z += length;
+                positionHasChanged = true;
+            }
+
+            if (positionHasChanged)
+            {
+                boatInstance.transform.localPosition = localPosition;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+
+        // Draw top border
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, 0f, length));
+    }
+}
